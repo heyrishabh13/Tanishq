e35f273e5275834bb4c8332ca171d52e92368dd5
Rishabh Kamboj <heyrishabh13@gmail.com>
2021-11-01 18:35:47 +0530

    Update data.js

--- data/data.js
+++ data/data.js
@@ -122,16 +122,6 @@ const products = [
         occasion: "Traditional and Ethnic Wear",
         product: "Pendant"
     },
-    {
-        name: "Opulent Mangalsutra Pendant",
-        newPrice: 62170,
-        oldPrice: 62867,
-        images: ["https://staticimg.titan.co.in/Tanishq/Catalog/511156PRVAAA00_1.jpg?impolicy=pqmed&imwidth=640", "https://staticimg.titan.co.in/Tanishq/Catalog/511156PRVAAA00_2.jpg?impolicy=pqmed&imwidth=640", "https://staticimg.titan.co.in/Tanishq/Catalog/511156PRVAAA00_10.jpg?impolicy=pqmed&imwidth=640"],
-        purity: "22.00",
-        jewelleryType: "Plain Gold Jewellery",
-        occasion: "Traditional and Ethnic Wear",
-        product: "Pendant"
-    }
 
 
 ]
