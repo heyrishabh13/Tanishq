478ff9a3d25c869a95c5a8f75c6fabc665218407
Gajanan-Watt <wattamwargaj@gmail.com>
2021-11-01 18:49:23 +0530

    one more product

--- data/data.js
+++ data/data.js
@@ -231,6 +231,16 @@ const products = [
         jewelleryType: "Plain Jewellery with Stones",
         occasion: "Casual Wear",
         product: "Earrings"
+    },
+    {
+        name: "Intricate Enchanting Gold Jhumkas",
+        newPrice: 155957,
+        oldPrice: 159879,
+        images: ["https://staticimg.titan.co.in/Tanishq/Catalog/51O1PPJCUABA00_1.jpg?impolicy=pqmed&imwidth=640"],
+        purity: "22.00",
+        jewelleryType: "Plain Gold Jewellery",
+        occasion: "Traditional and Ethnic Wear",
+        product: "Earrings"
     }
 
 
